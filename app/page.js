5d0ba3f36546e9b68f6309448ed45ad14053d120
Secrets Room <hamomido2490@gmail.com>
2025-08-23 05:08:53 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <h1 className="text-4xl text-neonBlue font-bold">
-        Chamber of Secrets | غرفة الأسرار
-      </h1>
-    </div>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="flex items-center justify-center min-h-screen bg-black">
+        <h1 className="text-4xl text-neonBlue font-bold">
+          Chamber of Secrets | غرفة الأسرار
+        </h1>
+      </div>
+    </>
   );
 }
