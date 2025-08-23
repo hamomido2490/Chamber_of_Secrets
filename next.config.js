9df8026a5420a166b66bc1f965a7e4d209bcb9f1
Secrets Room <hamomido2490@gmail.com>
2025-08-24 01:19:36 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    useLightningcss: true // تعطيل lightningcss عشان مشاكل 32-bit
+    useLightningcss: true
   }
 };
 
